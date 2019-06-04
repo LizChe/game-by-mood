726cf4137c24723be4bb744feb93652f14620e98
Liz <pachaffy@gmail.com>
2019-06-04 21:08:45 +0200

    Implement GamesContainer

---
+++ src/main/java/com/codecool/gamebymood/model/GamesContainer.java
@@ -0,0 +1,11 @@
+package com.codecool.gamebymood.model;
+
+public class GamesContainer {
+
+    public static final String ADVENTEROUS = "https://thewitcher.com/en/witcher3";
+    public static final String COMPETETIVE = "http://onrushgame.com";
+    public static final String LONELY = "https://www.ea.com/games/unravel/unravel-two";
+    public static final String PATIENT = "https://www.crashbandicoot.com";
+    public static final String THOUGHTFULL = "https://www.hellblade.com";
+    public static final String WISE = "https://www.playstation.com/en-gb/games/knowledge-is-power-ps4";
+}
