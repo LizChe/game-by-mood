7c7e1e4ad74d9ea35745976abf803efbe993d8a5
Liz <pachaffy@gmail.com>
2019-06-05 21:07:14 +0200

    Fix typos in variable names

--- src/main/java/com/codecool/gamebymood/model/GamesContainer.java
+++ src/main/java/com/codecool/gamebymood/model/GamesContainer.java
@@ -2,10 +2,10 @@ package com.codecool.gamebymood.model;
 
 public class GamesContainer {
 
-    public static final String ADVENTEROUS = "https://thewitcher.com/en/witcher3";
-    public static final String COMPETETIVE = "http://onrushgame.com";
+    public static final String ADVENTUROUS = "https://thewitcher.com/en/witcher3";
+    public static final String COMPETITIVE = "http://onrushgame.com";
     public static final String LONELY = "https://www.ea.com/games/unravel/unravel-two";
     public static final String PATIENT = "https://www.crashbandicoot.com";
-    public static final String THOUGHTFULL = "https://www.hellblade.com";
+    public static final String THOUGHTFUL = "https://www.hellblade.com";
     public static final String WISE = "https://www.playstation.com/en-gb/games/knowledge-is-power-ps4";
 }
