06ecf89ae9abd4003757f733d6a219bd3f181564
Liz <pachaffy@gmail.com>
2019-06-06 13:48:59 +0200

    Put fields of GamesContainer into a hashmap

--- src/main/java/com/codecool/gamebymood/model/GamesContainer.java
+++ src/main/java/com/codecool/gamebymood/model/GamesContainer.java
@@ -1,11 +1,27 @@
 package com.codecool.gamebymood.model;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class GamesContainer {
 
-    public static final String ADVENTUROUS = "https://thewitcher.com/en/witcher3";
-    public static final String COMPETITIVE = "http://onrushgame.com";
-    public static final String LONELY = "https://www.ea.com/games/unravel/unravel-two";
-    public static final String PATIENT = "https://www.crashbandicoot.com";
-    public static final String THOUGHTFUL = "https://www.hellblade.com";
-    public static final String WISE = "https://www.playstation.com/en-gb/games/knowledge-is-power-ps4";
+    private static final String ADVENTUROUS = "https://thewitcher.com/en/witcher3";
+    private static final String COMPETITIVE = "http://onrushgame.com";
+    private static final String LONELY = "https://www.ea.com/games/unravel/unravel-two";
+    private static final String PATIENT = "https://www.crashbandicoot.com";
+    private static final String THOUGHTFUL = "https://www.hellblade.com";
+    private static final String WISE = "https://www.playstation.com/en-gb/games/knowledge-is-power-ps4";
+    private static final String NO_CHOICE = "https://www.minecraft.net/en-us/";
+
+    public Map<String, String> getGames() {
+        Map<String, String> games = new HashMap<>();
+        games.put("adventurous", ADVENTUROUS);
+        games.put("competitive", COMPETITIVE);
+        games.put("lonely", LONELY);
+        games.put("patient", PATIENT);
+        games.put("thoughtful", THOUGHTFUL);
+        games.put("wise", WISE);
+        games.put("no choice", NO_CHOICE);
+        return games;
+    }
 }
